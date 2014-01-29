cbe93a21f5254b4b9146162b57dab4f377d8f639
richard.delorenzi <none@none>
2014-01-29 23:30:50 +0000

    another json helper

--- Json.cs
+++ Json.cs
@@ -10,6 +10,10 @@ namespace Richard.Json
 			return o==null?null:JObject.FromObject(o).ToString();
 		}
 
+		public static string ToJsonString(this JObject o){
+			return o==null?null:o.ToString();
+		}
+
 		public static string AsString(this JObject o, string key)
 		{
 			var v = o[key];
